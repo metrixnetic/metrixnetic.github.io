152c320946e0a7ec231b00471cb64f964bab62a8
LGarikL <garik.margaryan.07@mail.ru>
2020-06-20 10:14:14 +0400

    Lamp-Fix 2.0

--- ts-js/lamp-sound.js
+++ ts-js/lamp-sound.js
@@ -9,29 +9,34 @@ let clickCount = 0
 
 $("#svg").on("click",function enable(){
   document.getElementById("all").style.filter = "brightness(10%)";
+    document.getElementById("Svg-logo").style.filter = "brightness(10%)";
   document.getElementById("bg").style.display = "none";
    $("form").css("visibility","hidden");
 
   $("#svg").on("click",function enable(){
   document.getElementById("all").style.filter = "brightness(100%)";
+   document.getElementById("Svg-logo").style.filter = "brightness(100%)";
   document.getElementById("bg").style.display = "block";
 
   $("#svg").on("click",function enable(){
   document.getElementById("all").style.filter = "brightness(10%)";
+   document.getElementById("Svg-logo").style.filter = "brightness(10%)";
   document.getElementById("bg").style.display = "none";
    $("form").css("visibility","hidden");
 
   $("#svg").on("click",function enable(){
-  document.getElementById("all").style.filter = "brightness(5%)";
-  document.getElementById("bg").style.display = "none";
+  document.getElementById("all").style.filter = "brightness(100%)";
+        document.getElementById("Svg-logo").style.filter = "brightness(100%)";
+  document.getElementById("bg").style.display = "block";
  
   $("form").css("visibility","hidden");
    if(clickCount===4){
-   	 document.getElementById("all").style.filter = "brightness(5%)";
+   	 document.getElementById("all").style.filter = "brightness(10%)";
+      document.getElementById("Svg-logo").style.filter = "brightness(10%)";
   document.getElementById("bg").style.display = "none";
   alert("Well, well, well, looks like you need to pay for the light.")
     $("form").css("visibility","visible");
-    $(this).css("opacity","0");
+    $(".close").css("visibility","visible");
     
     //если нужно каждый 4 клик ловить, то:
     clickCount = 0
@@ -39,5 +44,12 @@ $("#svg").on("click",function enable(){
     clickCount+=1
   }
 
+$(".close").on("click",function clickedbtn() {
+  document.getElementById("all").style.filter = "brightness(100%)";
+        document.getElementById("Svg-logo").style.filter = "brightness(100%)";
+  document.getElementById("bg").style.display = "block";
+  $(".close").css("visibility","hidden");
+  $("form").css("visibility","hidden");
 
+}); 
 });   }); }); }); 
