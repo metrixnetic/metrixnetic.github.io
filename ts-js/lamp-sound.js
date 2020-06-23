700ef530105072c8eb209f9e8411bb38391783e6
LGarikL <garik.margaryan.07@mail.ru>
2020-06-23 13:38:18 +0400

    SuperPuperGiperMegaExtraNothing

--- ts-js/lamp-sound.js
+++ ts-js/lamp-sound.js
@@ -1,6 +1,7 @@
+ 
 var audio = $("#bear")[0];
 $("#svg").click(function(){
-	audio.play();
+  audio.play();
 });
 
 $("form").css("visibility","hidden");
@@ -10,7 +11,6 @@ let clickCount = 0
 $("#svg").on("click",function enable(){
   document.getElementById("ole").style.filter = "brightness(10%)";
   document.getElementById("bg").style.display = "none";
-   $("form").css("visibility","hidden");
 
   $("#svg").on("click",function enable(){
   document.getElementById("ole").style.filter = "brightness(100%)";
@@ -19,8 +19,6 @@ $("#svg").on("click",function enable(){
   $("#svg").on("click",function enable(){
   document.getElementById("ole").style.filter = "brightness(10%)";
   document.getElementById("bg").style.display = "none";
-   $("form").css("visibility","hidden");
-
   $("#svg").on("click",function enable(){
   document.getElementById("ole").style.filter = "brightness(100%)";
   document.getElementById("bg").style.display = "block";
@@ -28,17 +26,16 @@ $("#svg").on("click",function enable(){
     $("#svg").on("click",function enable(){
   document.getElementById("ole").style.filter = "brightness(10%)";
   document.getElementById("bg").style.display = "none";
-   $("form").css("visibility","hidden");
 
 
    if(clickCount===4){
-   	 document.getElementById("ole").style.filter = "brightness(100%)";
+     document.getElementById("ole").style.filter = "brightness(100%)";
+     $(".close").css("visibility","hidden");
   document.getElementById("bg").style.display = "block";
   var elem = alert("Well, well, well, looks like you need to pay for the light.");
   delete alert 
-    $("form").css("visibility","visible");
+       document.getElementById("form").style.visibility = 'visible';
     $(".close").css("visibility","visible");
-
     if(clickCount = 0);
   }else{
     clickCount+=1
@@ -48,7 +45,7 @@ $(".close").on("click",function clickedbtn() {
   document.getElementById("ole").style.filter = "brightness(100%)";
   document.getElementById("bg").style.display = "block";
   $(".close").css("visibility","hidden");
-  $("form").css("visibility","hidden");
+document.getElementById("form").style.display = "none";
 
 }); 
 }); }); }); }); }); 
